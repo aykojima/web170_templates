72c501c00036bb4c0b02b706532fe5836b713633
aykojima <ayumikjm@gmail.com>
2017-04-14 12:22:10 -0700

    more commit

---
+++ commodities.php
@@ -0,0 +1,19 @@
+<?php include ("includes/top.php");?>
+
+   
+<!-- Products explanation-->    
+    <div class="productsAbout">
+    <p>We customize harvest to meet your needs. </p>
+    <p>Destroy couch as revenge hide from vacuum cleaner. Put toy mouse in food bowl run out of litter box at full speed favor packaging over toy sleep on dog bed, force dog to sleep on floor but claws in your leg or lick the plastic bag and meow flee in terror at cucumber discovered on floor. Chase imaginary bugs behind the couch, but please stop looking at your phone and pet me, thinking longingly about tuna brine chase the pig around the house for lick yarn hanging out of own butt. Russian blue. Stare at the wall, play with food and get confused by dust step on your keyboard while you're gaming and then turn in a circle , hide from vacuum cleaner hopped up on catnip pee in human's bed until he cleans the litter box pelt around the house and up and down stairs chasing phantoms. Lie on your belly and purr when you are asleep.
+</p>
+    
+    </div>
+<!-- Product pictures and links -->    
+    <div class="products">
+    <a href="#" onMouseover="chgImg('button4','button4Red')" onMouseout= "chgImg('button4','button4Blue')"><img src="images/broccoli.jpg"  name="button4"  alt="broccoli" class="products"></a>
+    <a href="#" onMouseover="chgImg('button5','button5Red')" onMouseout= "chgImg('button5','button5Blue')"><img src="images/iceberg.jpg"  name="button5"  alt="iceberg" class="products"></a>    
+    <a href="#" onMouseover="chgImg('button6','button6Red')" onMouseout= "chgImg('button6','button6Blue')"><img src="images/romaine.jpg"  name="button6"  alt="romaine" class="products"></a>
+    <a href="#" onMouseover="chgImg('button7','button7Red')" onMouseout= "chgImg('button7','button7Blue')"><img src="images/kale.jpg"  name="button7"  alt="kale" class="products"></a>
+    </div>
+
+<?php include ("includes/bottom.php");?>
