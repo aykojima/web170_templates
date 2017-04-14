10b5b140744fb467285b9ca2f18c8325ce14e2ce
aykojima <ayumikjm@gmail.com>
2017-04-14 12:18:53 -0700

    about and contact.php

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php include ("includes/top.php");?>
+
+<!-- Main picture-->
+    
+<!-- three pictures-->    
+<main>
+<section class="aboutPic">
+    <img src="images/about.jpg" alt="image" title="tentative imate" id="about">
+</section>
+    
+<article class="about">
+<h3>About SunAmerica Farms</h3>
+    <p>Destroy couch as revenge hide from vacuum cleaner. Put toy mouse in food bowl run out of litter box at full speed favor packaging over toy sleep on dog bed, force dog to sleep on floor but claws in your leg or lick the plastic bag and meow flee in terror at cucumber discovered on floor. Chase imaginary bugs behind the couch, but please stop looking at your phone and pet me, thinking longingly about tuna brine chase the pig around the house for lick yarn hanging out of own butt. Russian blue. Stare at the wall, play with food and get confused by dust step on your keyboard while you're gaming and then turn in a circle , hide from vacuum cleaner hopped up on catnip pee in human's bed until he cleans the litter box pelt around the house and up and down stairs chasing phantoms. Lie on your belly and purr when you are asleep. </p>
+    
+    <p>Cats go for world domination hola te quiero leave dead animals as gifts, but attack feet lounge in doorway. Unwrap toilet paper meowing non stop for food and ignore the squirrels, you'll never catch them anyway asdflkjaertvlkjasntvkjn (sits on keyboard) for eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap for lick sellotape or touch water with paw then recoil in horror. Leave dead animals as gifts knock over christmas tree for stare at ceiling chase the pig around the house or sit on the laptop. Put toy mouse in food bowl run out of litter box at full speed chase mice. Has closed eyes but still sees you lick the other cats sweet beast chase after silly colored fish toys around the house pee in human's bed until he cleans the litter box eat a plant, kill a hand and please stop looking at your phone and pet me. </p>
+</article>
+</main>
+<?php include ("includes/bottom.php");?>
---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php include ("includes/top.php");?>
+
+<!-- Main picture-->
+<!-- contact form-->    
+    <div class="form">
+      	<h2>Contact Us</h2>
+		<!--<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other 1!       
+         *
+         */
+        include 'includes/simple.php'; #demonstrates a simple contact form
+        //include 'includes/multiple.php';#demonstrates multiple form elements
+
+	?>-->
+    </div>
+<!-- map and address -->
+    <div class="map">
+    <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.5354109136533!2d-121.67262968471327!3d36.68566997997226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808df8d45c0b4b99%3A0x3adb9f68d7b24312!2sQuail+Run+Cir%2C+Salinas%2C+CA+93907!5e0!3m2!1sen!2sus!4v1471628314815' width='400' height='300' style='border:0' allowfullscreen></iframe>	
+	</div>
+<h3>Office address:</h3>
+<p>19 Quail Run Circle Suite B,<br>Salinas, Ca 93907</p>
+<h3>Main office:</h3>
+<p>831.422.3894</p>
+<h3>Email:</h3>
+<p>ralph@sunamericaproduce.com</p>
+</div>    
+<?php include ("includes/bottom.php");?>
